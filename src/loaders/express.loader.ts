83e2413bc48dc52cb8d78f85e802e7980f910511
Alberto Beiztegui Casado <albertobeiz@gmail.com>
2022-10-03 12:50:06 +0200

    cors test

--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -17,36 +17,35 @@ export default ({ app }: IExpress): void => {
   })
 
   /**
-  * A middleware that allows cross-origin resource sharing.
-  */
+   * A middleware that allows cross-origin resource sharing.
+   */
   app.use(
     cors({
-      origin: [appConfig.FRONT_URL, appConfig.DEV_URL],
       allowedHeaders: ['Content-Type', 'Authorization']
     })
   )
 
   /**
-  * This is a middleware that sets the header if-none-match to no-match-for-this.
-  */
+   * This is a middleware that sets the header if-none-match to no-match-for-this.
+   */
   app.use(function (req, res, next) {
     req.headers['if-none-match'] = 'no-match-for-this'
     next()
   })
 
   /**
-    * A middleware that parses the body of the request and makes it available in the req.body object.
-    */
+   * A middleware that parses the body of the request and makes it available in the req.body object.
+   */
   app.use(express.json())
 
   /**
-    * A middleware that is used to prefix all the routes with the API prefix.
-    */
+   * A middleware that is used to prefix all the routes with the API prefix.
+   */
   app.use(appConfig.API.PREFIX, routes())
 
   /**
-  * This is a middleware that is used to catch all the routes that are not defined in the application.
-  */
+   * This is a middleware that is used to catch all the routes that are not defined in the application.
+   */
   app.use((req: IRequest, res: Response, next: NextFunction) => {
     next({
       message: 'Not Found',
@@ -62,17 +61,14 @@ export default ({ app }: IExpress): void => {
      * Handle 401 thrown by express-jwt library
      */
     if (err.name === 'UnauthorizedError') {
-      return res
-        .status(err.status)
-        .send({ message: err.message })
-        .end()
+      return res.status(err.status).send({ message: err.message }).end()
     }
     return next(err)
   })
 
   /**
-  * This is a middleware that is used to catch all the routes that are not defined in the application.
-  */
+   * This is a middleware that is used to catch all the routes that are not defined in the application.
+   */
   app.use((err: ResponseError, req: IRequest, res: Response, next: NextFunction) => {
     res.status(err.status || 500)
     res.json({
